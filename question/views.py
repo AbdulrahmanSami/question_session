bf04d3260b8b8e8e7756bf138937fae63ab21c25
AbdulrahmanSami <abo55od@hotmail.com>
2017-04-20 07:54:40 +0300

    Second

--- question/views.py
+++ question/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from .models import Question_sessions,Question
+from .models import QuestionSession,Question
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators import csrf
 import json
 def show_questioning_session(request, pk):
-    question_session = get_object_or_404(Question_sessions, pk=pk)
+    question_session = get_object_or_404(QuestionSession, pk=pk)
     question_session.question_set.all()
     last_pk = Question.objects.last().pk
     return render(request, "question/index.html", {'question_session': question_session,
                                                    "last_pk": last_pk})
 @csrf.csrf_exempt
 def handle_questioning_ajax(request, pk):
-    question_session = get_object_or_404(Question_sessions, pk=pk),
+    question_session = get_object_or_404(QuestionSession, pk=pk),
     old_last_pk = request.POST['last_pk']
     new_last_pk = Question.objects.last().pk
     if old_last_pk != new_last_pk:
-        new_questions = question_session.question.objects.filter(id__gt=new_last_pk)
+        new_questions = question_session.question.objects.filter(id__gt=old_last_pk)
         response= {'new_questions':new_questions}
     else:
         pass
